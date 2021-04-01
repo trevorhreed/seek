278516a23121e2c61e4032f18894b3da7df43671
Trevor <trevorhreed@gmail.com>
2021-04-01 11:22:22 -0600

    more updates

--- docs/script.js
+++ docs/script.js
@@ -2,22 +2,32 @@
 
   const searchSplitRegex = /[\s\b]+/g
   const alphaOnlyRegex = /[^a-z\s]/g
+  const verseAndChapterRegex = /(.+?)(\d+):(:?\d+)$/g
   const [
-    verseJson,
+    indexJson,
+    verseMap,
     structure
   ] = await Promise.all([
-    fetch('./verses.json').then(res => res.json()),
+    fetch('./index.json').then(res => res.json()),
+    fetch('./verse-map.json').then(res => res.json()),
     fetch('./structure.json').then(res => res.json())
   ])
-  const verses = verseJson.map(x => {
-    x.lowerText = x.text.toLowerCase().replace(alphaOnlyRegex, '')
-    x.lowerTokens = x.lowerText.split(searchSplitRegex).filter(Boolean)
-    return x
-  })
-  const search = document.querySelector('#search')
+  const index = lunr.Index.load(indexJson)
+  const verses = []
+  Object
+    .keys(verseMap)
+    .forEach(ref => {
+      const [_, book, chapter, verse] = verseAndChapterRegex.exec(ref) || []
+      verseMap[ref].book = book
+      verseMap[ref].chapter = chapter
+      verseMap[ref].verse = verse
+      verses.push(verseMap[ref])
+    })
+
+  const searchInput = document.querySelector('#search')
   const main = document.querySelector('main')
   const aside = document.querySelector('aside')
-  const works = document.querySelector('#works')
+  const worksContainer = document.querySelector('#works')
   const books = document.querySelector('#books')
   const resultsEl = document.querySelector('.results')
   const list = document.querySelector('#list')
@@ -28,66 +38,45 @@
   const templateVerse = document.querySelector('#verse')
   const templateChapter = document.querySelector('#chapter')
 
-  // works.append(
-  //   ...structure.map(work => {
-  //     const div = document.createElement('DIV')
-  //     div.textContent = work.name
-  //     return div
-  //   })
-  // )
+  let selectedWork = null
+  const workElements = []
+  const onWorkClick = e => {
+    const { target } = e
+    const { work } = target.dataset
+    workElements.forEach(div => {
+      if (div === target) return
+      div.classList.remove('selected')
+    })
+    const isSelected = target.classList.toggle('selected')
+    selectedWork = isSelected ? work : null
+    filterResults()
+  }
+  worksContainer.append(
+    ...structure.map(work => {
+      const div = document.createElement('DIV')
+      div.classList.add('work')
+      div.textContent = work.name
+      div.dataset.work = work.name
+      div.addEventListener('click', onWorkClick)
+      workElements.push(div)
+      return div
+    })
+  )
 
+
+  const filterResults = () => {
+    results = unfilteredResults.filter(item => {
+      return selectedWork === null || item.work === selectedWork
+    })
+    updateResults()
+  }
+
+  let unfilteredResults = []
   let results = []
   let resultOffset = 0
 
-  const THRESHOLD = .001
-  const EXACT_MATCH_MODIFIER = 1
-  const STARTS_WITH_MODIFIER = .9
-  const FULL_CONTAINS_MODIFIER = .7
-  const PARTIAL_CONTAINS_MODIFIER = .3
-  const seek = (term, arr) => {
-    term = term.replace(alphaOnlyRegex, '').toLowerCase()
-    const terms = term.split(searchSplitRegex).filter(Boolean)
-    const items = []
-    arr.forEach(item => {
-      const value = item.lowerText
-      const tokens = item.lowerTokens
-      const termRatio = terms.length / tokens.length
-      const exactMatchScore = EXACT_MATCH_MODIFIER * (value === term ? 1 : 0)
-      const startsWithScore = STARTS_WITH_MODIFIER * (value.startsWith(term) ? termRatio : 0)
-      const fullContainsScore = FULL_CONTAINS_MODIFIER * (value.includes(term) ? termRatio : 0)
-      const partialContainsScore = PARTIAL_CONTAINS_MODIFIER * tokens
-        .map(token => {
-          const exactMatchCount = EXACT_MATCH_MODIFIER * (terms.filter(x => token === x).length / terms.length)
-          const startsWithCount = STARTS_WITH_MODIFIER * (terms.filter(x => token.startsWith(x)).length / terms.length)
-          const containsCount = FULL_CONTAINS_MODIFIER * (terms.filter(x => token.includes(x)).length / terms.length)
-          return (exactMatchCount + startsWithCount + containsCount) / 3
-        })
-        .reduce((a, c) => a + c) / tokens.length
-      items.push({
-        ref: item.ref,
-        work: item.work,
-        book: item.book,
-        chapter: item.chapter,
-        verse: item.verse,
-        text: item.text,
-        relevance: Math.max(
-          exactMatchScore,
-          startsWithScore,
-          fullContainsScore,
-          partialContainsScore
-        )
-      })
-    })
-    return items
-      .filter(x => x.relevance > THRESHOLD)
-      .sort((a, b) => {
-        if (a.relevance > b.relevance) return -1
-        if (a.relevance < b.relevance) return 1
-        return 0
-      })
-  }
-
   const gatherChapter = (book, chapter) => {
+    console.dir(verses)
     return verses.filter(x => x.book === book && x.chapter === chapter)
   }
 
@@ -121,7 +110,8 @@
   }
 
   const clearResults = () => {
-    results = []
+    selectedWork = null
+    unfilteredResults = results = []
     resultOffset = 0
     loading.style.display = 'none'
     while (list.lastChild) {
@@ -141,6 +131,7 @@
         linkEl.textContent = result.ref
         linkEl.dataset.book = result.book
         linkEl.dataset.chapter = result.chapter
+        linkEl.dataset.terms = searchInput.value
         linkEl.addEventListener('click', openChapter)
         textEl.textContent = result.text
         return div
@@ -154,37 +145,46 @@
   document.addEventListener('keyup', e => {
     if (e.target.tagName === 'INPUT') return
     if (e.key === '/') {
-      search.focus()
-      search.select()
+      searchInput.focus()
+      searchInput.select()
     }
   })
 
-  function debounce(func, timeout = 300) {
+  const debounce = (fn, timeout = 300) => {
     let timer
     return (...args) => {
       clearTimeout(timer)
-      timer = setTimeout(() => { func.apply(this, args) }, timeout)
+      timer = setTimeout(() => {
+        fn.apply(this, args)
+      }, timeout)
     }
   }
 
   const handleInput = e => {
-    if (!search.value) {
+    if (!searchInput.value) {
       clearResults()
       loading.style.display = 'none'
     }
-    // if (e.key !== 'Enter') return
     clearResults()
-    results = seek(search.value, verses)
+    unfilteredResults = results = index
+      .search(searchInput.value)
+      .map(({ ref }) => verseMap[ref])
+      updateResults()
+  }
+
+  const updateResults = () => {
+    resultOffset = 0
+    loading.style.display = 'none'
+    while (list.lastChild) {
+      list.removeChild(list.lastChild)
+    }
     loading.style.display = 'flex'
     composeResults()
     resultsEl.scrollTo({ top: 0, behavior: 'smooth' })
   }
 
-  search.focus()
-  search.addEventListener('keyup', e => {
-    loading.style.display = 'flex'
-    debounce(handleInput)(e)
-  })
+  searchInput.focus()
+  searchInput.addEventListener('keyup', debounce(handleInput))
 
   close.addEventListener('click', e => {
     aside.style.display = 'none'
