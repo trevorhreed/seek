23556b8f6fabddb3aeb43aabfd12af46af3d0d26
Trevor <trevorhreed@gmail.com>
2021-07-18 14:35:37 -0600

    adding theme detection

--- src/scripts.js
+++ src/scripts.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', async e => {
     resultTemplate,
     verseTemplate
   ] = querySelector(
-    '.search input',
+    '.search',
     'main',
     ['.works div'],
     '.results',
